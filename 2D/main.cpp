c6445fce0be7d1521b05cfd61986b71b3515b1cf
Cyril C <cyclaus73@gmail.com>
2025-03-17 11:07:33 +0000

    Ajout cas 2D

--- 2D/main.cpp
+++ 2D/main.cpp
@@ -5,6 +5,26 @@
 using namespace std;
 using namespace mfem;
 
-int main() {
+static real_t e = 0.001f;
+
+
+int main(int argc, char* argv[])
+{   
+    // Il faut passer deux arguments lors de l'execution du programme
+    int n  = 4;       // Nombre d'éléments du maillage (4 par défaut)
+    int order = 1;    // Ordre des fonctions de test (1 par défaut)  
+
+    cout << argc;
+
+    if (argv[1]) {
+        n = stoi(argv[1]);
+    }
+    cout << argv[2];
+
+    if(argv[2]) {
+        order = stoi(argv[2]);
+    }
+    // Création du maillage 
+    // Mesh mesh =  Mesh::MakeCartesian2D(n, n,);
     return 0;
 }
