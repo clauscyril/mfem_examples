600fcc1bc8267ef0495fb0c5d34dbf7d11ec79b5
Cyril C <cyclaus73@gmail.com>
2025-03-17 13:15:21 +0000

    Edit 2D case

--- 2D/main.cpp
+++ 2D/main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char* argv[])
     int n  = 4;       // Nombre d'éléments du maillage (4 par défaut)
     int order = 1;    // Ordre des fonctions de test (1 par défaut)  
 
-    cout << argc;
+    // cout << argc;
 
     if (argv[1]) {
         n = stoi(argv[1]);
@@ -25,6 +25,23 @@ int main(int argc, char* argv[])
         order = stoi(argv[2]);
     }
     // Création du maillage 
-    Mesh mesh =  Mesh::MakeCartesian2D(n, n, Element::TRIANGLE, true, 1, 1);
+    Mesh mesh = Mesh::MakeCartesian2D(n, n, Element::TRIANGLE, true, 1, 1);
+
+    ofstream mesh_ofs("refined.mesh");
+    mesh_ofs.precision(8);
+    mesh.Print(mesh_ofs);
+
+
+
+
+    // Affichage du maillage
+    char vishost[] = "localhost";
+    int  visport   = 19916;
+    socketstream sol_sock(vishost, visport);
+    // socketstream sol_sock_i(vishost, visport);
+    sol_sock.precision(8);
+    // sol_sock_i.precision(8);
+    sol_sock << "solution\n" << mesh << "window_title 'Mesh'";  // Génère une erreur dans glvis à corriger    
+
     return 0;
 }
