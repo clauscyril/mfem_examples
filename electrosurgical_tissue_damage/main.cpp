be7d6130771249a663646911978031a8ee52325e
Woody <cyclaus73@gmail.com>
2025-05-05 12:30:11 +0200

    Segmentation du code

--- electrosurgical_tissue_damage/main.cpp
+++ electrosurgical_tissue_damage/main.cpp
@@ -3,100 +3,16 @@
 // #include <algorithm>
 
 #include <mfem.hpp>
+#include "solver.hpp"  // Solver personnalisé pour le problème
+
 
 using namespace mfem;
 using namespace std;
 
 
-real_t q_func(const Vector &x, real_t &t)
-{
-    if (t < 1000)
-        if ((x(0) < -0.45) or (x(0) > 0.45))
-            return 1.;
-        else 
-            return 0.;
-    else 
-        return 0;
-}
-
-class DG_Solver : public Solver
-{
-private:
-   SparseMatrix &M, &K, A;
-   GMRESSolver linear_solver;
-   BlockILU prec;
-   real_t dt;
-public:
-   DG_Solver(SparseMatrix &M_, SparseMatrix &K_, const FiniteElementSpace &fes)
-      : M(M_),
-        K(K_),
-        prec(fes.GetTypicalFE()->GetDof(),
-             BlockILU::Reordering::MINIMUM_DISCARDED_FILL),
-        dt(-1.0)
-   {
-        linear_solver.iterative_mode = false;
-        linear_solver.SetRelTol(1e-9);
-        linear_solver.SetAbsTol(0.0);
-        linear_solver.SetMaxIter(100);
-        linear_solver.SetPrintLevel(0);
-        linear_solver.SetPreconditioner(prec);
-   }
-
-   void SetTimeStep(real_t dt_)
-    {
-        if (dt_ != dt)
-        {
-            dt = dt_;
-            // Form operator A = M - dt*K
-            A = K;
-            A *= -dt;
-            A += M;
-
-            // this will also call SetOperator on the preconditioner
-            linear_solver.SetOperator(A);
-        }
-    }
-
-   void SetOperator(const Operator &op) override
-    {
-        linear_solver.SetOperator(op);
-    }
-
-   void Mult(const Vector &x, Vector &y) const override
-   {
-        linear_solver.Mult(x, y);
-   }
-};
-
-/** A time-dependent operator for the right-hand side of the ODE. The DG weak
-    form of du/dt = -v.grad(u) is M du/dt = K u + b, where M and K are the mass
-    and advection matrices, and b describes the flow on the boundary. This can
-    be written as a general ODE, du/dt = M^{-1} (K u + b), and this class is
-    used to evaluate the right-hand side. */
-    class FE_Evolution : public TimeDependentOperator
-    {
-    private:
-       BilinearForm &M, &K;
-       const Vector &b;
-       Solver *M_prec;
-       CGSolver M_solver;
-       DG_Solver *dg_solver;
-    
-       mutable Vector z;
-    
-    public:
-       FE_Evolution(BilinearForm &M_, BilinearForm &K_, const Vector &b_);
-    
-       void Mult(const Vector &x, Vector &y) const override;
-       void ImplicitSolve(const real_t dt, const Vector &x, Vector &k) override;
-    
-       ~FE_Evolution() override;
-    };
-    
-
 int main() {
 
-    int vis_steps = 1000;
+    int vis_steps = 100;
     bool visualization = true;
     int precision = 8;
 
@@ -110,7 +26,8 @@ int main() {
     real_t rho_times_c_ = 3.9e6;
     // real_t rho_times_c_ = 1e6;
 
-    real_t k_ = 0.533;
+    // real_t k_ = 0.533;
+    real_t k_ = 10;
 
     unique_ptr<ODESolver> ode_solver = ODESolver::Select(4); // Runge kuta
 
@@ -169,7 +86,7 @@ int main() {
 
     q.Assemble(); 
 
-
+    
     socketstream sout;
 
     char vishost[] = "localhost";
@@ -185,7 +102,7 @@ int main() {
     else
     {
         sout.precision(precision);
-        sout << "solution\n" << mesh << T;
+        sout << "solution\n" << mesh << T << "\nkeys j\n";
         sout << "pause\n";
         sout << flush;
         cout << "GLVis visualization paused."
@@ -222,70 +139,18 @@ int main() {
 
         if (done || ti % vis_steps == 0)
         {
-            cout << "time step: " << ti << ", time: " << t << endl;
-            
+            cout << "Time step: " << ti << ", Time: " << t << endl;
+        
             if (visualization)
             {
-                sout << "solution\n" << mesh << T << flush;
-                // std::cin.get(); 
+                sout.precision(8);
+                sout << "solution\n" << mesh << T
+                     << "window_title 'Temperature'"
+                     << flush;
+                // std::cin.get(); // Décommenter pour pause manuelle
             }
-        
         }
     }
 
     return 0;
-}
-
-
-
-
-
-// Implementation of class FE_Evolution
-FE_Evolution::FE_Evolution(BilinearForm &M_, BilinearForm &K_, const Vector &b_)
-   : TimeDependentOperator(M_.FESpace()->GetTrueVSize()),
-     M(M_), K(K_), b(b_), z(height)
-{
-   Array<int> ess_tdof_list;
-   if (M.GetAssemblyLevel() == AssemblyLevel::LEGACY)
-   {
-      M_prec = new DSmoother(M.SpMat());
-      M_solver.SetOperator(M.SpMat());
-      dg_solver = new DG_Solver(M.SpMat(), K.SpMat(), *M.FESpace());
-   }
-   else
-   {
-      M_prec = new OperatorJacobiSmoother(M, ess_tdof_list);
-      M_solver.SetOperator(M);
-      dg_solver = NULL;
-   }
-   M_solver.SetPreconditioner(*M_prec);
-   M_solver.iterative_mode = false;
-   M_solver.SetRelTol(1e-9);
-   M_solver.SetAbsTol(0.0);
-   M_solver.SetMaxIter(100);
-   M_solver.SetPrintLevel(0);
-}
-
-void FE_Evolution::Mult(const Vector &x, Vector &y) const
-{
-   // y = M^{-1} (K x + b)
-   K.Mult(x, z);
-   z += b;
-   M_solver.Mult(z, y);
-}
-
-void FE_Evolution::ImplicitSolve(const real_t dt, const Vector &x, Vector &k)
-{
-   MFEM_VERIFY(dg_solver != NULL,
-               "Implicit time integration is not supported with partial assembly");
-   K.Mult(x, z);
-   z += b;
-   dg_solver->SetTimeStep(dt);
-   dg_solver->Mult(z, k);
-}
-
-FE_Evolution::~FE_Evolution()
-{
-   delete M_prec;
-   delete dg_solver;
 }
