1d36352e759c1b7a495e66dc6f4356080faadb11
Woody <cyclaus73@gmail.com>
2025-07-24 11:34:19 +0200

    small changes

--- Loss Computation in Ferrites/2D/Time_Domain/Reduction POD/plot_error.py
+++ Loss Computation in Ferrites/2D/Time_Domain/Reduction POD/plot_error.py
@@ -3,29 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
-steps = np.linspace(0,990,100)
-
 path = os.path.dirname(os.path.abspath(__file__))
 path = os.path.join(path, "build", "data")
-
-
-errors = []
-
-# for el in steps :
-#     step = str(int(el))
-#     path_fom = os.path.join(path, "fom", "Hn_"+step+"0.csv")
-#     path_rom = os.path.join(path, "reduced", "Hn_reduced_"+step+"0")
-
-#     data_fom = pd.read_csv(path_fom)
-#     Hn_fom = data_fom['Hn']
-
-#     data_rom = pd.read_csv(path_rom, header=None)
-#     Hn_rom = data_rom[0]
-#     diff = Hn_fom - Hn_rom
-#     error = np.linalg.norm(diff)
-#     error_ref = np.linalg.norm(Hn_fom)
-#     errors.append(error/error_ref)
-
 
 path_fom = os.path.join(path, "fom", "TD_0.csv")
 path_rom = os.path.join(path, "reduced", "TD_1.csv")
@@ -33,31 +12,20 @@ path_rom = os.path.join(path, "reduced", "TD_1.csv")
 data_fom = pd.read_csv(path_fom, sep=";")
 data_rom = pd.read_csv(path_rom, sep=";")
 
-print(data_rom)
-
 flux_fom = data_fom["flux"]
 flux_rom = data_rom["flux"]
 diff_flux = flux_fom - flux_rom
 
-
 p_eddy_fom = data_fom["p_eddy"]
 p_eddy_rom = data_rom["p_eddy"] 
-
-# alpha = 1.4
-# p_eddy_rom_reduced = p_eddy_rom_reduced * alpha
 
 power_error = abs(p_eddy_rom - p_eddy_fom)
 
 t = data_rom['t']
+NI = data_fom["NI"]
 
 print(f"Power Fom :  {np.mean(p_eddy_fom[200:])}")
 print(f"Power ROM reduced : {np.mean(p_eddy_rom[200:])}")
-
-# plt.plot(errors)
-# plt.title("Relative Error between Fom and Rom")
-# plt.xlabel("Iteration")
-# plt.ylabel("Relative error")
-# plt.grid()
 
 plt.figure()
 
@@ -71,14 +39,21 @@ plt.legend()
 
 plt.figure()
 plt.plot(t, diff_flux, label="Difference Flux (FOM - ROM)")
-
 plt.grid()
 plt.xlabel("Time (s)")
 plt.ylabel("Magnetic flux (Wb)")
 plt.legend()
 
-plt.figure()
 
+plt.figure()
+plt.plot(t, NI, label="NI")
+plt.grid()
+plt.xlabel("Time (s)")
+plt.ylabel("NI (A)")
+plt.legend()
+
+
+plt.figure()
 # plt.plot(t, p_eddy_rom, 'black', label="Power Rom")
 plt.plot(t, p_eddy_rom, 'green', label="Power Rom Reduced ")
 plt.plot(t, p_eddy_fom, 'r-.', label="Power FOM")
